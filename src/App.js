8a98fb61cc20d6bfea5616c3035466a7627cae6c
Nana Kweku <nanakwekudev@gmail.com>
2022-04-19 15:11:12 +0000

    bootstrap react added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import './App.css';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path='/'>
+              <Route index element={<Home/>}/>
+              
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavbarComp from '../components/navbar/NavbarComp'
+
+const Home = () => {
+  return (
+    <div className='home'>
+        <NavbarComp></NavbarComp>
+    </div>
+  )
+}
+
+export default Home
