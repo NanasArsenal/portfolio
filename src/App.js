34c4b247e8e9c2baad8f7ed0d0cc2fa7ba480733
Nana Kweku <nanakwekudev@gmail.com>
2022-04-20 19:00:08 +0000

    starting from scratch

--- src/App.js
+++ src/App.js
@@ -4,14 +4,7 @@ import Home from './pages/Home/Home';
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path='/'>
-              <Route index element={<Home/>}/>
-              
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      App
     </div>
   );
 }
